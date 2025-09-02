99fad1b963cc46154698c8dd5f0872529a6e5afe
ykie-k <124432301+ykie-k@users.noreply.github.com>
2025-09-02 11:07:46 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -16,9 +16,9 @@
     </n-flex>
     <n-flex :size="4" class="text" align="center" vertical>
       <n-p depth="3">
-        <n-text depth="3" @click="jumpLink(linkData.github)">
-          SiteStatus
-        </n-text>
+        <n-text depth="3" @click="jumpLink(metaData.project)">
+          SiteStatus
+        </n-text>
         Version {{ version }}
       </n-p>
       <n-p depth="3">
@@ -32,7 +32,7 @@
       </n-p>
       <n-p depth="3">
         Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        <n-text depth="3" @click="jumpLink(metaData.author)"> IMSYY </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
@@ -50,9 +50,13 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/ykie-k/site-status",
+  home: "https://www.174982.xyz",
+  email: "yukejie@174982.xyz",
+};
+const metaData = {
+  author: "https://imsyy.top",
+  project: "https://github.com/imsyy/site-status",
 };
 </script>
 
